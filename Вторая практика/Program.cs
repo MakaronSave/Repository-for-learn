73b6fcd590e3435ad40e3bf728aa9fff19d6e9b1
Matvey Shekh <shekhmatvey@yandex.by>
2023-08-07 14:25:18 +0300

    + Вторая практика

---
+++ Вторая практика/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Первая_практика_Задание__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money, food, inFoodUnitPrise;
+            bool isAbleToPay;
+            inFoodUnitPrise = 10;
+            Console.WriteLine($"Добрый день, сегодняшняя цена на еду: {inFoodUnitPrise} монет.");
+            Console.WriteLine("Сколько у вас золота?");
+            Console.Write("У меня золота: ");
+            money = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Сколько вы хотите купить еды?");
+            Console.Write("Я хотел бы купить еды в количестве: ");
+            food = Convert.ToInt32(Console.ReadLine());
+
+
+            isAbleToPay = money >= food * inFoodUnitPrise;
+            food *= Convert.ToInt32(isAbleToPay);
+            money -= food * inFoodUnitPrise;
+            Console.Write($"Вы купили еды: {food} едениц, у вас осталось {money} монет.");
+            Console.ReadLine();
+
+        }
+    }
+}
