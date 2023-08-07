533c2782661ff75ca6c84b38f6aed3583d1ad9ea
Matvey Shekh <shekhmatvey@yandex.by>
2023-08-07 19:02:49 +0300

    6 практика

---
+++ Практики/6 практика/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Шестая_практика
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float money;
+            int years;
+            int percent;
+
+            Console.Write("Введите количество денег, внесённых на вклад: ");
+            money = Convert.ToSingle(Console.ReadLine());
+            Console.Write("На сколько лет открыт вклад? ");
+            years = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Под какой процент открыт вклад: ");
+            percent = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < years; i++)
+            {
+                money += money / 100 * percent;
+                Console.WriteLine("В этом году у вас " + money);
+                Console.ReadKey();
+            }
+        }
+    }
+}
