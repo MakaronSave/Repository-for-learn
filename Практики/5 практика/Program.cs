8536529563a9b8b335dfc362e98286d9ee207f51
Matvey Shekh <shekhmatvey@yandex.by>
2023-08-07 17:32:00 +0300

    Пятая практикааааа

--- Практики/5 практика/Program.cs
+++ Практики/5 практика/Program.cs
@@ -17,14 +17,16 @@ namespace Пятая_практика
             string password = Console.ReadLine();
             /// Эта строка ЮзерИнпут будет тем неправильным паролем
             string userInput;
-            for (int i = 0; i <= tryPassword; i++)
+            for (int i = 0; i < tryPassword; i++)
             {
                 Console.Write("Введите пароль, который вы только что задали(ведь вы же его забыли, да?): ");
                 userInput = Console.ReadLine();
-                if (password == userInput) { break;
-                    Console.WriteLine("Пароль был введён правильно, кто бы сомневался.");
+                if (password == userInput)
+                {
+                    Console.WriteLine("Поздравляю, вы всё таки списали пароль правильно!");
+                    break;
                 }
-                if (i == tryPassword) { Console.WriteLine("Попытки кончились, перезагрузите программу."); }
+                if (i == tryPassword - 1) { Console.WriteLine("Попытки кончились, перезагрузите программу."); }
             }
             Console.ReadLine();
         }
