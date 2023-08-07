98ea365a19cb3e431696c1dd5c92aee6b1913733
Matvey Shekh <shekhmatvey@yandex.by>
2023-08-07 05:08:55 +0300

    Первая практика

---
+++ Первая практика/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Первая_практика
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int health, armor, damage;
+            Console.Write($"Введите количество здоровья: ");
+            health = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Введите процент блокирования урона");
+            armor = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Введите количество урона: ");
+            damage = Convert.ToInt32(Console.ReadLine());
+            health -= damage * armor / 100;
+            // Я не буду записывать 100 как переменную, т.к это не обязательно. Магическое число всё таки это магическое число.
+            Console.WriteLine($"Вам нанесли {damage} урона, у вас осталось {health} здоровья.");
+            // Не совсем понял, зачем мне делать практику, потому что он сам там всё решает прям на видео, так что я сильно не стараюсь :D
+        }
+    }
+}
